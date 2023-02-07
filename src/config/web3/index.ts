cc406fd4a15b16557d0b632c4c39210c2b28c125
Alex <dev1@conectate.com.gt>
2023-02-07 08:35:10 -0600

    change cahinID to Goerli

--- src/config/web3/index.ts
+++ src/config/web3/index.ts
@@ -9,7 +9,8 @@ import Web3 from "web3/dist/web3.min";
 import { InjectedConnector } from "@web3-react/injected-connector";
 const connector = new InjectedConnector({
 	supportedChainIds: [
-		4 // Rinkeby
+		4, // Rinkeby
+		5 // Goerli
 	]
 });
 
